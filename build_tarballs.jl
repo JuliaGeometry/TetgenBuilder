8cc440aed8ca72ff504e3ddb4359e90f6e262e6e
SimonDanisch <sdanisch@gmail.com>
2019-03-09 14:10:38 +0100

    shared library build

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -2,6 +2,9 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
+name = "TetGen"
+version = v"1.5"
+
 # Collection of sources required to build tetgen
 sources = [
     "https://github.com/ufz/tetgen.git" =>
@@ -12,42 +15,27 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd tetgen/
-mkdir build
-cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
-make
-mkdir ../../../destdir/bin/
-for f in tetgen*; do mv "$f" "../../../destdir/bin/$f"; done
+${CXX} -fPIC -O0 -c predicates.cxx
+${CXX} -fPIC -DTETLIBRARY -c tetgen.cxx
+libdir="lib"
+if [[ ${target} == *-mingw32 ]]; then     libdir="bin"; else     libdir="lib"; fi
+mkdir ${prefix}/${libdir}
+${CXX} $LDFLAGS -shared -fPIC tetgen.o predicates.o -o ${prefix}/${libdir}/libtet.${dlext}
 exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
-    Linux(:powerpc64le, :glibc),
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms = BinaryBuilder.supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
-    ExecutableProduct(prefix, "", :tetgen)
+    LibraryProduct(prefix, "libtet", :libtet)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "tetgen", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
